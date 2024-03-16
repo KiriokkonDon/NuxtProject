3a47a26a2b1a9bfff33d9c33e8b2e707e76b71af
KiriokkonDon <akane.vip@mail.ru>
2024-03-16 22:47:57 +1100

    Task3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
+    
+    modules: [
       [
-          "@nuxtjs/google-fonts",
-          {
-              families: {
-                  Montserrat: true,
-                  "Open Sans": true,
-              },
+        "@nuxtjs/google-fonts",
+        {
+          families: {
+            Montserrat: true,
+            "Open Sans": true,
           },
+        },
       ],
-  ],
-  devtools: { enabled: true },
-});
+    ],
+    devtools: { enabled: true },
+  });
+  
